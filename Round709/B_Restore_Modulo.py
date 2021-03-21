68f878c3e9e6334e15e6f404119597d32b21f940
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2021-03-21 17:10:48 +0100

    Finish round 709

---
+++ Round709/B_Restore_Modulo.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+import os
+import sys
+from io import BytesIO, IOBase
+import math
+from collections import Counter
+
+
+def func(array):
+    # print(array)
+    n = len(array)
+    if n == 1:
+        return 0
+    max_val = max(array)
+    max_index = array.index(max_val)
+    increase = False
+    if max_index > 0:
+        c = array[max_index] - array[max_index - 1]
+    else:
+        c = None
+        for i in range(n - 1):
+            if array[i] <= array[i + 1]:
+                c = array[i + 1] - array[i]
+                break
+        if c is None:
+            c = array[max_index + 1] - array[max_index]
+    # print("c", c)
+    if c <= 0:
+        m = max_val + 1
+    elif max_index < n - 1:
+        m = max_val + c - array[max_index + 1]
+        if m <= max_val:
+            return -1
+    else:
+        m = None
+        for i in range(n - 1):
+            if array[i] > array[i + 1]:
+                m = array[i] + c - array[i + 1]
+                if m <= max_val:
+                    return -1
+                break
+        if m is None:
+            increase = True
+            m = max_val + 1
+
+    # print(m, c)
+    for i in range(n - 1):
+        if array[i + 1] != (array[i] + c) % m:
+            return -1
+    if c <= 0 or increase:
+        return 0
+    return f"{m} {c}"
+
+
+def main():
+    num_test = int(parse_input())
+    result = []
+    for _ in range(num_test):
+        n = int(parse_input())
+        array = [int(i) for i in parse_input().split()]
+        result.append(func(array))
+    print("\n".join(map(str, result)))
+
+
+# region fastio
+
+# BUFSIZE = 8192
+
+
+# class FastIO(IOBase):
+#     newlines = 0
+
+#     def __init__(self, file):
+#         self._fd = file.fileno()
+#         self.buffer = BytesIO()
+#         self.writable = "x" in file.mode or "r" not in file.mode
+#         self.write = self.buffer.write if self.writable else None
+
+#     def read(self):
+#         while True:
+#             b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+#             if not b:
+#                 break
+#             ptr = self.buffer.tell()
+#             self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+#         self.newlines = 0
+#         return self.buffer.read()
+
+#     def readline(self):
+#         while self.newlines == 0:
+#             b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+#             self.newlines = b.count(b"\n") + (not b)
+#             ptr = self.buffer.tell()
+#             self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+#         self.newlines -= 1
+#         return self.buffer.readline()
+
+#     def flush(self):
+#         if self.writable:
+#             os.write(self._fd, self.buffer.getvalue())
+#             self.buffer.truncate(0), self.buffer.seek(0)
+
+
+# class IOWrapper(IOBase):
+#     def __init__(self, file):
+#         self.buffer = FastIO(file)
+#         self.flush = self.buffer.flush
+#         self.writable = self.buffer.writable
+#         self.write = lambda s: self.buffer.write(s.encode("ascii"))
+#         self.read = lambda: self.buffer.read().decode("ascii")
+#         self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+# sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+parse_input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+if __name__ == "__main__":
+    main()
