c468bf3841ff099c46e14604e8c4fd7a817bf7c1
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2025-07-31 22:42:37 +0200

    Update

---
+++ Round1040/B_Pathless.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+import os
+import sys
+from io import BytesIO, IOBase
+import math
+from collections import Counter
+
+
+def func(n, s, array):
+    counts = Counter(array)
+    current_sum_all_elements = sum(array)
+
+    if s < current_sum_all_elements:
+        result_array = []
+        result_array.extend([0] * counts[0])
+        result_array.extend([1] * counts[1])
+        result_array.extend([2] * counts[2])
+        return " ".join(map(str, result_array))
+    elif s == current_sum_all_elements:
+        return -1
+    else:
+        K = s - current_sum_all_elements
+        if K == 1:
+            result_array = []
+            result_array.extend([0] * counts[0])
+            result_array.extend([2] * counts[2])
+            result_array.extend([1] * counts[1])
+            return " ".join(map(str, result_array))
+        else:
+            return -1
+
+
+def main():
+    num_test = int(parse_input())
+    result = []
+    for _ in range(num_test):
+        n_s_line = [int(i) for i in parse_input().split()]
+        n = n_s_line[0]
+        s = n_s_line[1]
+        array = [int(i) for i in parse_input().split()]
+        result.append(func(n, s, array))  # Pass 's' here
+    print("\n".join(map(str, result)))
+
+
+# region fastio
+
+# BUFSIZE = 8192
+
+
+# class FastIO(IOBase):
+#     newlines = 0
+
+#     def __init__(self, file):
+#         self._fd = file.fileno()
+#         self.buffer = BytesIO()
+#         self.writable = "x" in file.mode or "r" not in file.mode
+#         self.write = self.buffer.write if self.writable else None
+
+#     def read(self):
+#         while True:
+#             b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+#             if not b:
+#                 break
+#             ptr = self.buffer.tell()
+#             self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+#         self.newlines = 0
+#         return self.buffer.read()
+
+#     def readline(self):
+#         while self.newlines == 0:
+#             b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+#             self.newlines = b.count(b"\n") + (not b)
+#             ptr = self.buffer.tell()
+#             self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+#         self.newlines -= 1
+#         return self.buffer.readline()
+
+#     def flush(self):
+#         if self.writable:
+#             os.write(self._fd, self.buffer.getvalue())
+#             self.buffer.truncate(0), self.buffer.seek(0)
+
+
+# class IOWrapper(IOBase):
+#     def __init__(self, file):
+#         self.buffer = FastIO(file)
+#         self.flush = self.buffer.flush
+#         self.writable = self.buffer.writable
+#         self.write = lambda s: self.buffer.write(s.encode("ascii"))
+#         self.read = lambda: self.buffer.read().decode("ascii")
+#         self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+# sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+parse_input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+if __name__ == "__main__":
+    main()
